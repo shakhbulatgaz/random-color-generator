681b1d91fd32ccd410bcf63529885c6dd095ee05
Shakhbulat <shakhbulat@faunusaff.com>
2018-09-04 21:49:41 +0400

    Tweaks

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# random-color-generator  
+# colorrr  
+Library to generate random HEX and RGB colors.  
+~~~~    
+var colorrr = require("colorrr");  
+colorrr.getHex();  
+colorrr.getRBG();
+~~~~
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-module.getHex = function() {
-      let letters = '0123456789ABCDEF';
+exports.getHex = function() {
+      let characters = '0123456789ABCDEF';
       let color = '#';
       for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        color += characters[Math.floor(Math.random() * 16)];
       }
       return color;
-    }
+    };
+
+exports.getRGB = function() {
+    let r = () => { return Math.floor(Math.random() * 256) };
+    var color = 'rgb('+ r() +','+ r() +','+ r() +')';
+    return color;
+};
