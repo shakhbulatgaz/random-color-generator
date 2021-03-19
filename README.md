088a11f7eb07252134f9a06533b8549d494f12a4
sh.gazgireev <shakhbulatgaz@gmail.com>
2021-03-20 01:02:06 +0300

    Total refactoring and speed improvements.

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # colorrr  
-Library to generate random HEX and RGB colors.  
+## Library that generates random HEX and RGB colors.
+~~~~
+~260 bytes gzipped
+~~~~
+Use it like this:
 ~~~~    
 var colorrr = require("colorrr");  
-colorrr.getHex();  
-colorrr.getRBG();
+
+colorrr.getHex();  // "#28A0B9" (string)
+colorrr.getRBG();  // [122, 116, 46] (array)
+~~~~
+
+Or like this:
+~~~~
+import { getHex, getRGB } from 'colorrr';
+
+getHex(); // "#28A0B9" (string)
+getRGB(); // [122, 116, 46] (array)
 ~~~~
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const getHex = () => {
-      let characters = '0123456789ABCDEF';
-      let color = '#';
-      for (let i = 0; i < 6; i++) {
-        color += characters[Math.floor(Math.random() * 16)];
-      }
-      return color;
-    };
+export function getRandNumInRange(r) {
+  return Math.random() * r | 0;
+}
 
-const getRGB = () => {
-    let r = () => { return Math.floor(Math.random() * 256) };
-    let color = 'rgb('+ r() +','+ r() +','+ r() +')';
-    return color;
+export function getHex() {
+  let color = '';
+
+  for(let i = 0; color.length < 6; i++) {
+    color = color.concat('0123456789ABCDEF'[getRandNumInRange(16)]);
+  }
+
+  return `#${color}`;
 };
 
-export {getRGB, getHex};
+export function getRGB() {
+  const result = [];
+
+  while(result.length !== 3) {
+    result.push(getRandNumInRange(256));
+  }
+
+  return result;
+};
